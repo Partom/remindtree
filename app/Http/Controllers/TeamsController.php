9e28b841530090c1fbb377e53e7eb60be1b71748
Muhammad Asif <muhammadasif.mmma@gmail.com>
2016-12-02 17:07:04 +0500

    Minor Changes

--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Group;
+use App\Team;
 use Auth;
 use App\User;
 use Carbon\Carbon;
@@ -15,7 +15,7 @@ class TeamsController extends Controller
 
     public function index()
     {
-    	$teams = Group::where('owner_id', Auth::id())->orderby('created_at', 'DESC')->paginate(25);
+    	$teams = Team::where('owner_id', Auth::id())->orderby('created_at', 'DESC')->paginate(25);
         $date = Carbon::now();
     	return view('admin.teams.index', compact('teams'));
     }
