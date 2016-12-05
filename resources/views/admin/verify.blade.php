00525e2a2a29dd8ddea8f7050df2b3266cdd037c
Aashir <ashir.cool4@gmail.com>
2016-12-05 17:07:48 +0500

    minor resend email fix

--- resources/views/admin/verify.blade.php
+++ resources/views/admin/verify.blade.php
@@ -7,6 +7,7 @@
     <div class="col-md-12">
         <div class="white-box">
             <h3 class="box-title">You Have to verify your email</h3>
+            <a href="{{ url('/') }}/email-verification/check/{{Auth::user()->verification_token}}?email={{Auth::user()->email}}" class="btn btn-link">Resend email</a>
         </div>
     </div>
 </div>
